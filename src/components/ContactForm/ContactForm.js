6209e05c8c7fc81970ba0dcc7a52bc588b980862
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2023-10-26 17:58:45 +0300

    fox7

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -55,7 +55,7 @@ export const ContactForm = () => {
         <FormItem
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[^\d]+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           placeholder="Enter name"
@@ -66,7 +66,9 @@ export const ContactForm = () => {
         <FormItem
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+\d{12}"
+          minlength="13"
+          maxlength="13"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           placeholder="Enter phone number"
